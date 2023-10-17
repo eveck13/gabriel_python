c1baf2c4aae452e0af7eda0166b1d038191c4e4e
eveck13 <138641278+eveck13@users.noreply.github.com>
2023-10-16 19:43:41 -0600

    parte1

---
+++ #parte1.py
@@ -0,0 +1,6 @@
+# Importa las bibliotecas necesarias
+import json
+import yaml
+import xml.etree.ElementTree as ET
+# Crea una lista vacía para almacenar la información de las personas
+personas = []
