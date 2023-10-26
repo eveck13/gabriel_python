b2feca40c32b6637871d39cfbf70b5b1883204c1
eveck13 <138641278+eveck13@users.noreply.github.com>
2023-10-25 21:35:43 -0600

    Se agrego la parte de solicitud al usuario

--- #parte1.py
+++ #parte1.py
@@ -4,3 +4,19 @@ import yaml
 import xml.etree.ElementTree as ET
 # Crea una lista vacía para almacenar la información de las personas
 personas = []
+
+# Solicita al usuario la información de cada persona
+for i in range(10):
+    nombre = input("Ingrese el nombre de la persona {}: ".format(i + 1))
+    matricula = input("Ingrese la matrícula de la persona {}: ".format(i + 1))
+    especialidad = input("Ingrese la especialidad de la persona {}: ".format(i + 1))
+
+    # Crea un objeto persona con la información proporcionada
+    persona = {
+        "nombre": nombre,
+        "matrícula": matricula,
+        "especialidad": especialidad
+    }
+
+    # Agrega la persona a la lista
+    personas.append(persona)
